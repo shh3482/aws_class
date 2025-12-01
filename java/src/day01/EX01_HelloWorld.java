d7467c7cf4368a648cdfc469c58bc5c45a6fa837
shh3482 <shh3482@gmail.com>
2025-12-01 18:03:09 +0900

    Create EX01_HelloWorld.java

---
+++ java/src/day01/EX01_HelloWorld.java
@@ -0,0 +1,12 @@
+package day01;
+
+// 실행 방법 : Run 메뉴 > Run 클릭(단축키 : Ctrl + F11)
+// 주석 단축키 : 한줄 주석(Ctrl + /) 여러줄 주석(블록 지정 후 Ctrl + Shift + /)
+public class EX01_HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
