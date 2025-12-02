7ff95beff63add632ea60f190531f03574937c73
shh3482 <shh3482@gmail.com>
2025-12-02 18:20:57 +0900

    Create HW01_LCM.java

---
+++ java/src/day02/HW01_LCM.java
@@ -0,0 +1,22 @@
+package day02;
+
+public class HW01_LCM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* 두 정수의 최소 공배수를 구하는 코드를 작성하세요.
+		 * A의 배수 : 어떤 수를 A로 나누었을 때 나머지가 0인 수
+		 * A와 B의 공배수 : A와 B의 배수들 중 공통으로 있는 수
+		 * A와 B의 최소 공배수 : A와 B의 공배수 중 가장 작은 수
+		 */
+		int num1 = 2;
+		int num2 = 4;
+		for(int i = 1; i <= num1 * num2 ; i++) {
+			if(i == num1 * num2) {
+				System.out.println(num1 + " 와 " + num2 + " 의 최소 공배수는 " + i + " 입니다.");
+				break;
+			}
+		}
+	}
+
+}
