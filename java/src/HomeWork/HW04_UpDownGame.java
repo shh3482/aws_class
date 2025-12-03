5749a70c3f3742d620101e0e61a3c0122c779d9e
shh3482 <shh3482@gmail.com>
2025-12-03 18:22:24 +0900

    Create HW04_UpDownGame.java

---
+++ java/src/HomeWork/HW04_UpDownGame.java
@@ -0,0 +1,21 @@
+package HomeWork;
+
+public class HW04_UpDownGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* 1~100 사이의 랜덤한 수를 생성해서 맞추는 게임
+		 * 랜덤한 수 : 33 //안보여야 함
+		 * 정수입력 : 50
+		 * DOWN
+		 * 정수입력 : 25
+		 * UP
+		 * 정수입력 : 30
+		 * UP
+		 * 정수입력 : 33
+		 * 정답입니다.
+		 * 
+		 */
+	}
+
+}
