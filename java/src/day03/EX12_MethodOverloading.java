790fb11ac844e555ecff0229cff68a18b1907d97
shh3482 <shh3482@gmail.com>
2025-12-03 18:22:02 +0900

    Create EX12_MethodOverloading.java

---
+++ java/src/day03/EX12_MethodOverloading.java
@@ -0,0 +1,15 @@
+package day03;
+
+public class EX12_MethodOverloading {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* 메서드 오버로딩
+		 * - 동일한 이름의 메서드가 여러개 있는 경우
+		 * - 조건 : 매개변수가 다름. 둘 중 하나를 만족하면 됨
+		 *   1. 매개변수의 종류가 다름
+		 *   2. 매개변수의 개수가 다름
+		 */
+	}
+
+}
