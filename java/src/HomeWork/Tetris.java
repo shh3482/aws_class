51c524b97c529febc492b7a3ab90ffc544b2df26
shh3482 <shh3482@gmail.com>
2025-12-09 17:57:20 +0900

    대거변경

---
+++ java/src/HomeWork/Tetris.java
@@ -0,0 +1,56 @@
+package HomeWork;
+
+public class Tetris {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//□ ■ ▣ ┏ ━ ┓ ┗ ┛ ┃
+
+		int col = 10;
+		int row = 12;
+		int curCol = 0;
+		int curRow = 0;
+		
+		// 좌 상
+		if(curCol == 0 && curRow == 0) {
+			System.out.print("┏━");
+		}
+		
+		// 상
+		for(curCol = 1; curCol < col; curCol++) {
+			System.out.print("━━");
+		}
+		
+		// 우 상
+		if(curCol == col && curRow == 0) {
+			System.out.println("━┓");
+		}
+		
+		// 좌 우
+		for(curRow = 0; curRow < row; curRow++) {
+			System.out.print("┃");
+			for (curCol = 0; curCol < col; curCol++) {
+				System.out.print("  ");
+			}
+			System.out.println("┃");
+		}
+		
+		// 좌 하
+		if(curRow == row) {
+			System.out.print("┗━");
+		}
+		
+		// 하
+		for(curCol = 1; curCol < col; curCol++) {
+			System.out.print("━━");
+		}
+		
+		// 좌 하
+		if(curRow == row) {
+			System.out.print("━┛");
+		}
+		
+	}
+}
+
