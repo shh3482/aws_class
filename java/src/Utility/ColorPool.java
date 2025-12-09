e02009631d1d657e938a34d730376724208883cb
shh3482 <63386461+shh3482@users.noreply.github.com>
2025-12-10 01:56:54 +0900

    수정

---
+++ java/src/Utility/ColorPool.java
@@ -0,0 +1,40 @@
+package Utility;
+
+public class ColorPool {
+
+    // RESET
+    public static final String RESET = "\033[0m";
+
+    // TEXT COLORS
+    public static final String BLACK = "\033[30m";
+    public static final String RED = "\033[31m";
+    public static final String GREEN = "\033[32m";
+    public static final String YELLOW = "\033[33m";
+    public static final String BLUE = "\033[34m";
+    public static final String PURPLE = "\033[35m";
+    public static final String CYAN = "\033[36m";
+    public static final String WHITE = "\033[37m";
+
+    // BRIGHT TEXT
+    public static final String BRIGHT_RED = "\033[91m";
+    public static final String BRIGHT_GREEN = "\033[92m";
+    public static final String BRIGHT_YELLOW = "\033[93m";
+    public static final String BRIGHT_BLUE = "\033[94m";
+    public static final String BRIGHT_PURPLE = "\033[95m";
+    public static final String BRIGHT_CYAN = "\033[96m";
+    public static final String BRIGHT_WHITE = "\033[97m";
+
+    // BACKGROUND
+    public static final String BG_RED = "\033[41m";
+    public static final String BG_GREEN = "\033[42m";
+    public static final String BG_YELLOW = "\033[43m";
+    public static final String BG_BLUE = "\033[44m";
+    public static final String BG_PURPLE = "\033[45m";
+    public static final String BG_CYAN = "\033[46m";
+    public static final String BG_WHITE = "\033[47m";
+
+    // STYLES
+    public static final String BOLD = "\033[1m";
+    public static final String UNDERLINE = "\033[4m";
+}
+
---
+++ java/src/Utility/Theme_0.java
@@ -0,0 +1,10 @@
+package Utility;
+
+public class Theme_0 {
+
+    public static final String HP = ColorPool.GREEN;
+    public static final String DAMAGE = ColorPool.RED;
+    public static final String CRIT = ColorPool.BRIGHT_YELLOW + ColorPool.BOLD;
+    public static final String TURN = ColorPool.CYAN;
+
+}
