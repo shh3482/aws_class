683f68951613acdaa9f8f953bfdb50a5be7f988a
shh3482 <shh3482@gmail.com>
2025-12-26 18:06:43 +0900

    a

---
+++ java/src/HomeWork/Subject.java
@@ -0,0 +1,64 @@
+package HomeWork;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Subject implements Serializable{
+
+	private static final long serialVersionUID = 123L;
+	
+	//학년(grade), 학기(semeter), 과목명(name)을 필드로 선언하세요. 
+	private int grade, semester;
+	private String name;
+	
+	//grade, semester, name의 getter와 setter를 추가
+	
+	
+	//toString 오버라이딩해서 1학년 1학기 국어 형태의 문자열이
+	//반환되도록 작성하세요. 
+	@Override
+	public String toString() {
+		return grade + "학년 " + semester + "학기 " + name;
+	}
+
+	//학년, 학기, 이름를 이용한 생성자를 추가하세요. 
+	public Subject(int grade, int semester, String name) {
+		this.grade = grade;
+		this.semester = semester;
+		this.name = name;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// 주소가 같은지 확인 => 같은 객체를 공유 => 무조건 참
+		if (this == obj)
+			return true;
+		// 비교 대상이 없음 => 비교 못함 => 무조건 거짓
+		if (obj == null)
+			return false;
+		// 비교 대상의 종류가 다름 => 비교를 대부분 못함 => 거짓
+//		if (getClass() != obj.getClass())
+//			return false;
+		// 과목이랑 과목 성적이랑 비교 가능하게 구현하고 싶다
+		if (obj instanceof SubjectScore) {
+			SubjectScore suScore = (SubjectScore) obj;
+			return this.equals(suScore.getSubject());
+		}
+		// 두 객체의 클래스가 같으면 지정된 필드 각각을 비교하여 같은지를 판별
+		Subject other = (Subject) obj;
+		return grade == other.grade && Objects.equals(name, other.name) && semester == other.semester;
+	}
+	
+	
+}
+
+
+
+
+
+
---
+++ java/src/HomeWork/SubjectScore.java
@@ -0,0 +1,57 @@
+package HomeWork;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SubjectScore implements Serializable{
+
+	private static final long serialVersionUID = 123L;
+	
+	//다음 필드를 선언하세요. 
+	//과목 정보, 성적 
+	private Subject subject;
+	private double score;
+	
+	//toString을 오버라이딩하세요
+	//1학년 1학기 국어 90점 형태로 
+	@Override
+	public String toString() {
+		return subject.toString() + " " + score + "점";
+	}
+	
+	//학년, 학기, 과목명, 성적을 이용한 생성자를 추가하세요. 
+	public SubjectScore(int grade, int semester, String name, double score) {
+		/*
+		subject = new Subject();
+		subject.setGrade(grade);
+		subject.setSemester(semester);
+		subject.setName(name);
+		*/
+		subject = new Subject(grade, semester, name);
+		this.score = score;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SubjectScore other = (SubjectScore) obj;
+		return Objects.equals(subject, other.subject);
+	}
+	
+}
+
+
+
+
+
+
