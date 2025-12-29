9e09263326e741f2fcc20c82084f037518b0b748
shh3482 <shh3482@gmail.com>
2025-12-29 17:51:51 +0900

    a

---
+++ spring boot/boot/src/main/java/kr/hi/boot/controller/PostController.java
@@ -0,0 +1,18 @@
+package kr.hi.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PostController {
+	
+	@GetMapping("/post/list")
+	public String postList() {
+		return "post/list";
+	}
+	
+	@GetMapping("/user/signup")
+	public String userSignup() {
+		return "user/signup";
+	}
+}
