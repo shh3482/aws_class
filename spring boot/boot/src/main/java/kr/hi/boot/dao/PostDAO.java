922fdc5bb383f14b9b40196c7ac4eb4ee48b91aa
shh3482 <shh3482@gmail.com>
2026-01-14 18:09:24 +0900

    a

--- spring boot/boot/src/main/java/kr/hi/boot/dao/PostDAO.java
+++ spring boot/boot/src/main/java/kr/hi/boot/dao/PostDAO.java
@@ -5,12 +5,15 @@ import java.util.ArrayList;
 import org.apache.ibatis.annotations.Param;
 
 import kr.hi.boot.model.dto.PostDTO;
+import kr.hi.boot.model.util.Criteria;
 import kr.hi.boot.model.vo.Board;
 import kr.hi.boot.model.vo.Post;
 
 public interface PostDAO {
 
 	ArrayList<Board> getBoardList();
+	
+	ArrayList<Post> getPostList(@Param("cri")Criteria cri);
 
 	boolean insertBoard(@Param("name")String name);
 
@@ -27,5 +30,8 @@ public interface PostDAO {
 	void insertPost(@Param("post")PostDTO post);
 
 	void updatePostView(@Param("num")int num);
+
+	int selectPostListCount(@Param("cri")Criteria cri);
+
 
 }
