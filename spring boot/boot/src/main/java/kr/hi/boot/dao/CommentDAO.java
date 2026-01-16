8281c40b0cfda683f846cdc8404ae224f3d5f2b6
shh3482 <shh3482@gmail.com>
2026-01-16 18:12:56 +0900

    a

--- spring boot/boot/src/main/java/kr/hi/boot/dao/CommentDAO.java
+++ spring boot/boot/src/main/java/kr/hi/boot/dao/CommentDAO.java
@@ -4,10 +4,20 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
 
+import kr.hi.boot.model.util.Criteria;
 import kr.hi.boot.model.vo.Comment;
 
 public interface CommentDAO {
 
-	List<Comment> selectComments(@Param("poNum")int poNum);
+	List<Comment> selectComments(@Param("poNum")int poNum, @Param("cri")Criteria cri);
 	//삭제되지 않은 댓글만 가져옴, 시오오리넘 기준 내림차순 시오넘 기준 오름차순
+
+	int selectCommentsCount(@Param("poNum")int poNum);
+
+	boolean insertComment(@Param("comment")Comment comment);
+
+	boolean deleteComment(@Param("coNum")int coNum);
+
+	Comment selectComment(@Param("coNum")int coNum);
+
 }
