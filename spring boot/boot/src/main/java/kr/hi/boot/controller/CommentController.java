11e13e99849a6176bb7830a13fcfa473dcb6c7d8
shh3482 <shh3482@gmail.com>
2026-01-16 18:12:56 +0900

    a

--- spring boot/boot/src/main/java/kr/hi/boot/controller/CommentController.java
+++ spring boot/boot/src/main/java/kr/hi/boot/controller/CommentController.java
@@ -2,19 +2,23 @@ package kr.hi.boot.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.hi.boot.model.dto.CommentResponseDTO;
+import kr.hi.boot.model.util.Criteria;
+import kr.hi.boot.model.util.CustomUser;
+import kr.hi.boot.model.util.PageMaker;
 import kr.hi.boot.model.vo.Comment;
-import kr.hi.boot.model.vo.Post;
 import kr.hi.boot.service.CommentService;
-import kr.hi.boot.service.PostService;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -29,13 +33,41 @@ public class CommentController {
 	// api/v1/posts/게시글번호/comments 임시로 null
 	@GetMapping("/posts/{num}/comments")
 	@ResponseBody
-	public ResponseEntity<List<Comment>> getComments(
-		@PathVariable("num")int poNum
+	public ResponseEntity<CommentResponseDTO> getComments(
+		@PathVariable("num")int poNum,
+		Criteria cri
 		) {
+		cri.setPerPageNum(5);
 		// 게시글 번호 가져옴
 		// 서비스한테 번호주고 댓글 목록 가져옴
-		List<Comment> list = commentService.getComments(poNum);
+		List<Comment> list = commentService.getComments(poNum, cri);
+		
+		//서비스에게 게시글 번호와 페이지 정보를 주면서 pagemaker 객체를 가져오라고 요청
+		PageMaker pm = commentService.getPageMaker(poNum, cri);
+		
 		// 댓글목록 화면에 출력
-		return ResponseEntity.ok(list);
+		CommentResponseDTO dto = new CommentResponseDTO(list, pm);
+		
+		return ResponseEntity.ok(dto);
+	}
+	
+	@PostMapping("/posts/{num}/comments")
+	@ResponseBody
+	public ResponseEntity<String> getCommentsPost(
+		@PathVariable("num")int postNum,
+		@RequestBody Comment comment,
+		@AuthenticationPrincipal CustomUser user) {
+		comment.setPostNum(postNum);
+		String result = commentService.insertComment(comment,user);
+		return ResponseEntity.ok(result);
+	}
+	
+	@DeleteMapping("/posts/{postNum}/comments/{coNum}")
+	public ResponseEntity<String> postsCommentsDelete(
+		@PathVariable("coNum") int coNum,
+		@AuthenticationPrincipal CustomUser user) {
+		String result = commentService.deleteComment(coNum,user);
+		
+		return ResponseEntity.ok(result);
 	}
 }
--- spring boot/boot/src/main/java/kr/hi/boot/service/CommentService.java
+++ spring boot/boot/src/main/java/kr/hi/boot/service/CommentService.java
@@ -6,6 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import kr.hi.boot.dao.CommentDAO;
+import kr.hi.boot.model.util.Criteria;
+import kr.hi.boot.model.util.CustomUser;
+import kr.hi.boot.model.util.PageMaker;
 import kr.hi.boot.model.vo.Comment;
 
 @Service
@@ -20,10 +23,66 @@ public class CommentService {
 
 	//다오한테 번호주고 댓글 목록 요청
 	//댓글 목록 반환
-	public List<Comment> getComments(int poNum) {
+	public List<Comment> getComments(int poNum, Criteria cri) {
 		// TODO Auto-generated method stub
-		List<Comment> list = commentDAO.selectComments(poNum);
+		List<Comment> list = commentDAO.selectComments(poNum,cri);
 		return list;
+	}
+
+	public PageMaker getPageMaker(int poNum, Criteria cri) {
+		// TODO Auto-generated method stub
+		// 한페이지네이션에서 최대 페이지수를 3개로 선언
+		int pageCount = 3;
+		// 다오에게 게시글번호를 주면서 전체 댓글 수를 가져오라고 요청
+		int count = commentDAO.selectCommentsCount(poNum);
+		// 최대 페이지수, 전체게시글수, 현재페이지 정보를 이용하여 PageMaker 객체를 생성
+		PageMaker pm = new PageMaker(pageCount, cri, count);
+		// 생성된 객체를 반환
+		return pm;
+	}
+
+	public String insertComment(Comment comment, CustomUser user) {
+		// TODO Auto-generated method stub
+		if(user == null) {
+			return "로그인이 필요한 서비스입니다.";
+		}
+		if(comment.getContent() == null ||
+			comment.getContent().isBlank()) {
+			return "댓글을 입력하세요.";
+		}
+		comment.setId(user.getUsername());
+		boolean result = commentDAO.insertComment(comment);
+		if(result) {
+			return "댓글을 등록했습니다.";
+		}
+		return "댓글을 등록하지 못했습니다.";
+	}
+
+	public String deleteComment(int coNum, CustomUser user) {
+		// TODO Auto-generated method stub
+		if(user == null) {
+			return "로그인이 필요한 서비스입니다.";
+		}
+		String id = user.getUsername();
+		if(!isWriter(coNum, id)) {
+			return "작성자가 아닙니다.";
+		}
+		
+		boolean res = commentDAO.deleteComment(coNum);
+		if(res) {
+			return "댓글을 삭제했습니다.";
+		}
+		return "댓글을 삭제하지 못했습니다.";
+		
+	}
+	private boolean isWriter(int coNum, String id) {
+		Comment comment = commentDAO.selectComment(coNum);
+		System.out.println(comment);
+		if(comment == null) {
+			return false;
+		}
+		String writer = comment.getId();
+		return writer.equals(id);
 	}
 
 }
