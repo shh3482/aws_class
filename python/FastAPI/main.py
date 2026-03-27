f4928735ce2a4fbd571f0420c45c9851d7aa7232
shh3482 <shh3482@gmail.com>
2026-03-27 09:37:56 +0900

    a

---
+++ python/FastAPI/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, UploadFile, Form
+import uvicorn
+import numpy as np
+import mnist_learning as ml
+import cv2
+import 감정학습 as tm
+
+app = FastAPI()
+
+@app.get('/')
+async def index():
+  return {"msg" : "Hello FastAPI"}
+
+@app.post('/image')
+async def image(file:UploadFile):
+  # file을 바이너리 데이터로 읽어옴
+  contents = await file.read()
+  # 바이너리를 numpy 배열로 변환
+  np_arr = np.frombuffer(contents, np.uint8)
+  # 배열을 이미지로 디코딩
+  img = cv2.imdecode(np_arr, cv2.IMREAD_GRAYSCALE)
+  # 예측
+  res = ml.predict_from_upload_file(img,28,28)
+  print(f'URL : /image')
+  return {"msg" : res}
+
+if __name__ == '__main__':
+  # reload=True 사용시 주의 사항
+  # 자동으로 변경 사항을 적용하면 시간이 오래 걸리는 경우 안 쓰는 것이 좋음
+  uvicorn.run('main:app', port=8000, reload=True)
+
+@app.post('/text')
+async def text(msg:str=Form(...)):
+  res = tm.predict(msg)
+  return {"msg" : '긍정' if res else '부정'}
---
+++ python/day13/mnist_learning.py
@@ -0,0 +1,81 @@
+import os
+import glob
+import cv2
+import numpy as np
+from sklearn.utils import shuffle
+from sklearn.svm import SVC
+import joblib as jl
+from sklearn.datasets import fetch_openml
+
+# 지정된 폴더에 가서 이미지를 가져오고
+# 가져온 이미지를 독립변수(행열배열)들과 종속변수(값배열)들로 반환
+# 이미지들을 저장할때 폴더/숫자(라벨)/이미지파일 하는게 보통
+
+def load_image_dataset(root_path:str, width:int, height:int):
+  
+  # root_path 안에 있는 폴더명을 가져옴 => 답지
+  sub_folders = [folder for folder in os.listdir(root_path) if os.path.isdir(os.path.join(root_path, folder))]
+  
+  images, labels = [], []
+  # 폴더명을 이용해서 이미지들을 가져옴
+  for folder in sub_folders:
+    file_path = os.path.join(root_path, folder, '*.*')
+    for file in glob.glob(file_path):
+      # 파일을 이미지로 읽음
+      img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+      # 읽어온 파일 크기를 조절
+      img = cv2.resize(img, (width,height))
+      # 행렬로 변환해서 리스트(images)에 추가
+      images.append(img.flatten())
+      # 라벨을 리스트(labels)에 추가
+      labels.append(str(folder))
+  # numpy 리스트로 변환
+  images, labels = np.array(images), np.array(labels)
+  # 정규화
+  images = images.astype('float32') / 255.0
+  
+  return images, labels
+
+def train_model_save_model(X,y, file_name:str = 'mnist_model.pkl'):
+  # 데이터를 섞음
+  X,y = shuffle(X,y)
+  # 모델 선정
+  model = SVC()
+  # 학습
+  model.fit(X,y)
+  # 학습한 모델을 저장
+  model_data = {
+		'model' : model
+	}
+  jl.dump(model_data, file_name)
+  
+def load_model_predict(image:str, width:int, height:int, file_name:str = 'mnist_model.pkl'):
+  model_data = jl.load(file_name)
+  model = model_data['model']
+  
+  # 이미지 로드 => 리사이즈 => 행렬 => 정규화 => 예측
+  img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+  img = cv2.resize(img,(width,height))
+  img = img.flatten()
+  img = img.astype('float32') / 255.0
+  # 예측할 때 예측 데이터를 리스트로 한번 더 감싸야 함
+  # 이걸 안하려면 reshape(1,-1)을 통해 1행짜리 2차원 리스트로 만들면 됨 위에 .reshape 제거후 => return model.predict([img])
+  return model.predict([img])
+
+def download_mnist(root_path):
+  mnist = fetch_openml('mnist_784', version=1, as_frame=False)
+  X, y = mnist['data'], mnist['target']
+  for i in range(10000):
+    image = X[i].reshape(28, 28).astype(np.uint8)
+    label = y[i]
+    os.makedirs(os.path.join(root_path, label), exist_ok=True)
+    cv2.imwrite(os.path.join(root_path, label, f'img_{i}.png'), image)
+  
+
+if __name__ == '__main__':
+  # download_mnist('day13/images')
+  # images, labels = load_image_dataset('day13/images', 28, 28)
+  # train_model_save_model(images, labels)
+  print(load_model_predict('day13/6.png', 28,28))
+  print(load_model_predict('day13/7.png', 28,28))
+  print(load_model_predict('day13/66.png', 28,28))
